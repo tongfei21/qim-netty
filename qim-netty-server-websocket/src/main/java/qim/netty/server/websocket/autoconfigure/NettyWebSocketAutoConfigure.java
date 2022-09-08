4e3b5d60e7fe142c47256d9a5caba44e028e5c0c
tongfei <tongfei>
2022-09-08 10:53:52 +0800

    去掉多余的包

--- qim-netty-server-websocket/src/main/java/qim/netty/server/websocket/autoconfigure/NettyWebSocketAutoConfigure.java
+++
@@ -1,6 +0,0 @@
-package qim.netty.server.websocket.autoconfigure;
-import qim.netty.server.websocket.annotating.EnableWebSocket;
-
-@EnableWebSocket
-public class NettyWebSocketAutoConfigure {
-}
--- qim-netty-server/src/main/java/qim/netty/server/handler/NettyHeartHandler.java
+++
@@ -1,9 +0,0 @@
-package qim.netty.server.handler;
-
-public class NettyHeartHandler {
-
-    public static void HeartHandler(String body){
-
-        //System.out.println("----------"+ body +"---------------");
-    }
-}
